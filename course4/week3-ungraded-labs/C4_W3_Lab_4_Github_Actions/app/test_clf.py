6d93838947072d132697556f995a38347984967f
anh-vunguyen <anhvunguyen.france@gmail.com>
2022-05-17 10:05:28 +0930

    Adding new classifier with scaling and new unit test for the pipeline and the first step of the pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
@@ -1,6 +1,19 @@
 import pickle
 from main import clf
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
+def test_pipeline_and_scaler():
+	# Check if clf is an instance of sklearn.pipeline.Pipeline
+	isPipeline = isinstance(clf, Pipeline)
+	assert isPipeline
+	
+	if isPipeline:
+		# Check the first step of pipeline is an instance of 
+		# sklearn.preprocessing.StandardScaler
+		first_step = [v for v in clf.named_steps.values()][0]
+		assert isinstance(first_step, StandardScaler)
+		
 
 def test_accuracy():
 
